b4dcaa05079fa0011774196dbff9fe717ed62262
norinori3 <minus15kg@minus15kg-no-MacBook-Pro.local>
2017-11-05 15:14:40 +0900

    refactoring.

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ harvest api v2 client
 # usage
 
 1) Get your ACCESS_TOKEN and ACCOUNT_ID from [here](https://id.getharvest.com/sessions/new?go_back=%2Fdevelopers)
-2) Replace these values on harvest.py your own
-3)
+2) Replace these values on conf.ini.sample your own, and rename conf.ini.sample to conf.ini
--- harvest.py
+++ harvest.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
+from ConfigParser import SafeConfigParser
+
+filename = 'conf.ini'
+parser = SafeConfigParser()
+parser.read(filename)
 
 API_URL = 'https://api.harvestapp.com/api/v2/%s'
-API_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
-API_ACCOUNT_ID = 'YOUR_ACCOUNT_ID'
+API_ACCESS_TOKEN = parser.get('user', 'api_access_token')
+API_ACCOUNT_ID = parser.get('user', 'api_account_id')
 
 class Harvest(object):
     def __init__(self):
@@ -12,17 +20,20 @@ class Harvest(object):
             'User-Agent': 'MyApp (hoge)'
         }
 
+    def __get(self, path):
+        return requests.get(url = API_URL % path, headers=self.__headers).json()
+
     def me(self):
-        return requests.get(url = API_URL % 'users/me', headers=self.__headers).json()
+        return self.__get(path = 'users/me')
 
     def get_user_by(self, user_id):
         path = 'users/%s' % user_id
-        return requests.get(url=API_URL % path, headers=self.__headers).json()
+        return self.__get(path = path)
 
     def get_project_assignments_by(self, user_id):
         path = 'users/%s/project_assignments' % user_id
-        return requests.get(url=API_URL % path, headers=self.__headers).json()
+        return self.__get(path = path)
 
     def get_task_assignments_by(self, project_id):
         path = 'projects/%s/task_assignments' % project_id
-        return requests.get(url = API_URL % path, headers=self.__headers).json()
+        return self.__get(path = path)
